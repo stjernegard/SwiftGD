5b2fecc184d5061be175ac592ca2f09ca377f1d1
Markus Winter <markus@wntr.me>
2018-02-02 13:55:49 +0100

    Adds missing comments to color and hex color exports

--- Sources/Color.swift
+++ Sources/Color.swift
@@ -1,27 +1,73 @@
 import Foundation
 
-// TODO: "Unify" color, or make it more protocol based
-// TODO: Support none alpha color
-
+/// A struct that stores color data and opacity (alpha).
 public struct Color {
 
+    /// The red component
     public var red: Double
+
+    /// The green component
     public var green: Double
+
+    /// The blue component
     public var blue: Double
+
+    /// The alpha component
     public var alpha: Double
 
-    /// <#Description#>
+    /// Initializes a new color instance from given component values.
     ///
     /// - Parameters:
-    ///   - red: <#red description#>
-    ///   - green: <#green description#>
-    ///   - blue: <#blue description#>
-    ///   - alpha: <#alpha description#>
+    ///   - red: The red component of the new color (clipped between 0...1)
+    ///   - green: The green component of the new color (clipped between 0...1)
+    ///   - blue: The blue component of the new color (clipped between 0...1)
+    ///   - alpha: The alpha component of the new color (clipped between 0...1)
     public init(red: Double, green: Double, blue: Double, alpha: Double) {
-        self.red = red
-        self.green = green
-        self.blue = blue
-        self.alpha = alpha
+        self.red = max(0.0, min(1.0, red))
+        self.green = max(0.0, min(1.0, green))
+        self.blue =  max(0.0, min(1.0, blue))
+        self.alpha =  max(0.0, min(1.0, alpha))
+    }
+}
+
+// MARK: Description
+
+extension Color: CustomStringConvertible, CustomDebugStringConvertible {
+
+    /// A textual representation of this instance.
+    public var description: String {
+        return debugDescription
+    }
+
+    /// A textual representation of this instance, suitable for debugging.
+    public var debugDescription: String {
+        return "<red: \(red), green: \(green), blue: \(blue), alpha: \(alpha)>"
+    }
+}
+
+// MARK: Equatable & Hashable
+
+extension Color: Hashable {
+
+    /// The hash value (the hex value of the color)
+    public var hashValue: Int {
+        return hex
+    }
+
+    /// Returns a Boolean value indicating whether two values are equal.
+    ///
+    /// Equality is the inverse of inequality. For any values `a` and `b`,
+    /// `a == b` implies that `a != b` is `false`.
+    ///
+    /// - Parameters:
+    ///   - lhs: A value to compare.
+    ///   - rhs: Another value to compare.
+    /// - Returns: `true` if both values are equal, `false` otherwise
+    public static func == (lhs: Color, rhs: Color) -> Bool {
+        return lhs.red == rhs.red
+            && lhs.green == rhs.green
+            && lhs.blue == rhs.blue
+            && lhs.alpha == rhs.alpha
     }
 }
 
@@ -29,48 +75,67 @@ public struct Color {
 
 extension Color {
 
+    /// Returns opaque red
     public static let red = Color(red: 1, green: 0, blue: 0, alpha: 1)
 
+    /// Returns opaque green
     public static let green = Color(red: 0, green: 1, blue: 0, alpha: 1)
 
+    /// Returns opaque blue
     public static let blue = Color(red: 0, green: 0, blue: 1, alpha: 1)
 
+    /// Returns opaque black
     public static let black = Color(red: 0, green: 0, blue: 0, alpha: 1)
 
+    /// Returns opaque white
     public static let white = Color(red: 1, green: 1, blue: 1, alpha: 1)
+
+    /// Returns transparent black
+    public static let transparent = Color(red: 0, green: 0, blue: 0, alpha: 0)
 }
 
 // MARK: LibGD
 
 extension Color {
 
+    /*
+     gd uses an inverted (0=opaque, 1=transparent) 7-bit alpha channel (2^7=128), as opposed to the 8-bit r/g/b channels.
+     This is taken into account in conversions from/to the normalized color that is represented by `this`.
+     See: `gdAlpha` and `init(libgd:)`
+     Reference: https://github.com/libgd/libgd/issues/132
+    */
+
+    /// The red component in libgd space
     internal var gdRed: Int32 {
         return Int32(red * 255)
     }
+
+    /// The green component in libgd space
     internal var gdGreen: Int32 {
         return Int32(green * 255)
     }
+
+    /// The blue component in libgd space
     internal var gdBlue: Int32 {
         return Int32(blue * 255)
     }
+
+    /// The alpha component in libgd space
     internal var gdAlpha: Int32 {
-        // gd uses an inverted (0=opaque, 1=transparent) 7-bit alpha
-        // channel (2^7=128), as opposed by the 8-bit r/g/b channels.
-        // https://github.com/libgd/libgd/issues/132
         return 127 - Int32(alpha * 127)
     }
 
-    /// <#Description#>
+    /// Initializes a new color instance from given libgd color representation.
     ///
-    /// - Parameter libgd: <#libgd description#>
+    /// This initializes (re-)inverts the alpha channel of the libgd color (which uses inverted
+    /// alpha channels by design) and normalizes it around 0.0 (transparent) and 1.0 (opaque).
+    ///
+    /// - Parameter libgd: The libgd color to create a normalized color from.
     internal init(libgd color: Int32) {
         self.init(
             red: Double((color >> 16) & 0xff) / 255,
             green: Double((color >> 8) & 0xff) / 255,
             blue: Double((color >> 0) & 0xff) / 255,
-            // gd uses an inverted (0=opaque, 1=transparent) 7-bit alpha
-            // channel (2^7=128), as opposed by the 8-bit r/g/b channels.
-            // // https://github.com/libgd/libgd/issues/132
             alpha: 1 - (Double((color >> 24) & 0xff) / 127)
         )
     }
@@ -79,6 +144,19 @@ extension Color {
 // MARK: Hexadecimal
 
 extension Color {
+
+    /// Returns the color as hex value (RGBA)
+    public var hex: Int {
+        return Int(red * 255) << 24
+            + Int(green * 255) << 16
+            + Int(blue * 255) << 8
+            + Int(alpha * 255) << 0
+    }
+
+    /// Returns the color as hex string, e.g "ffee33dd" (RGBA)
+    public var hexString: String {
+        return String(hex, radix: 16)
+    }
 
     /// Initializes a new `Color` instance of given hexadecimal color string.
     ///
@@ -143,8 +221,10 @@ extension Color {
     /// - Throws: `.invalidColor` if given string is not of proper length
     private static func sanitize(hex string: String, leadingAlpha: Bool) throws -> String {
 
-        // Drop leading "#" if applicable
-        var string = string.hasPrefix("#") ? String(string.dropFirst(1)) : string
+        // Drop whitespaces and newline characters as well as a leading "#" if applicable
+        var string = string
+            .trimmingCharacters(in: .whitespacesAndNewlines)
+            .replacingOccurrences(of: "#", with: "", options: .anchored)
 
         // Evaluate if short code w/wo alpha (e.g. `feb` or `feb4`). Double up the characters if so.
         if string.count == 3 || string.count == 4 {
