f6db6d72b95fea99b579900032f84b172c04b1df
Markus Winter <markus@wntr.me>
2018-02-05 17:59:57 +0100

    Adds default initializers to geometry to make them accessible

--- Sources/Geometry.swift
+++ Sources/Geometry.swift
@@ -13,6 +13,16 @@ public struct Point {
 
     /// The point at the origin (0,0).
     public static let zero = Point(x: 0, y: 0)
+
+    /// Initializes a new point at given coordinates.
+    ///
+    /// - Parameters:
+    ///   - x: The x-coordinate of the point.
+    ///   - y: The y-coordinate of the point.
+    public init(x: Int32, y: Int32) {
+        self.x = x
+        self.y = y
+    }
 }
 
 extension Point: Equatable {
@@ -43,6 +53,16 @@ public struct Size {
 
     /// Size whose width and height are both zero.
     public static let zero = Size(width: 0, height: 0)
+
+    /// Initializes a new size of given dimensions.
+    ///
+    /// - Parameters:
+    ///   - width: The width value of the size.
+    ///   - height: The height value of the size.
+    public init(width: Int32, height: Int32) {
+        self.width = width
+        self.height = height
+    }
 }
 
 extension Size: Comparable {
@@ -91,6 +111,16 @@ public struct Rectangle {
             x: origin.x + (size.width >> 1),
             y: origin.x + (size.height >> 1)
         )
+    }
+
+    /// Initializes a new rectangle at given point of given size.
+    ///
+    /// - Parameters:
+    ///   - origin: The origin of the rectangle.
+    ///   - size: The size of the rectangle.
+    public init(origin: Point, size: Size) {
+        self.origin = origin
+        self.size = size
     }
 }
 
